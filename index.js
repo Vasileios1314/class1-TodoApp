06bfc58a0051bd1c9a15557798053f996c770dd7
Vasileios1314 <vasilis.bobonis@hotmail.com>
2022-01-04 13:30:03 +0100

    adding localStorage

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const btn = document.querySelector(".btn");
-
 const addBtn = document.querySelector(".c-input-btn");
 const allTodos = document.querySelector(".body-todos");
+
+document.addEventListener('DOMContentLoaded', getTodos);
 
 // when i click the btn
 addBtn.addEventListener("click", () => {
@@ -12,10 +13,12 @@ addBtn.addEventListener("click", () => {
   const todo = document.createElement("span");
   todo.classList.add("todo");
   todo.innerHTML = `<div class="todo-checkbox">
-  <input class="${input.value}" type="checkbox" id="${input.value}" name="${input.value}" />
+  <input class="${input.value}" type="checkbox" id="checkbox-input" name="${input.value}" />
   <label for="${input.value}">${input.value}</label>
 </div>
 <div class="todo-bin">
+
+
   <svg
     width="15px"
     color="#007bff"
@@ -29,8 +32,13 @@ addBtn.addEventListener("click", () => {
   </svg>
 </div>`;
 
+saveLocalTodos(input.value) 
+
   allTodos.append(todo);
   //   append also to local storage
+  
+
+
   //   remove value from input
   input.value = "";
 
@@ -38,16 +46,19 @@ addBtn.addEventListener("click", () => {
   const bin = todo.querySelector(".todo-bin");
   bin.addEventListener("click", () => {
     todo.remove();
+    
     // remove also from local storage
+    removeLocalTodos(todoDis)
   });
-
   //   is checkbox checked?
+  
 });
 
 // clear all btn
 btn.addEventListener("click", () => {
   const todos = document.querySelectorAll(".todo");
   todos.forEach((todo) => todo.remove());
+  
 });
 
 // To do for next time: Local Storage
@@ -58,8 +69,66 @@ btn.addEventListener("click", () => {
 
 // for each item we add in in our todos we add it also in local storage
 // and the same for delete
-if (localStorage.getItem("myTodos")) {
-  console.log("local storage is there");
-} else {
-  console.log("local storage is NOT there");
+function saveLocalTodos(todo){
+  let todos;
+  if (localStorage.getItem("myTodos") === null) {
+    todos = [];
+    console.log("local storage is there");
+  } else {
+    todos = JSON.parse(localStorage.getItem('myTodos'))
+    console.log("local storage is NOT there");
+  }
+  todos.push(todo)
+  localStorage.setItem('myTodos', JSON.stringify(todos))
+}
+function getTodos(){
+  let todos;
+  if (localStorage.getItem("myTodos") === null) {
+    todos = [];
+    console.log("local storage is there");
+  } else {
+    todos = JSON.parse(localStorage.getItem('myTodos'))
+    console.log("local storage is NOT there");
+  }
+  todos.forEach(function(todoDis){
+    const todo = document.createElement("span");
+    todo.classList.add("todo");
+    todo.innerHTML = `<div class="todo-checkbox">
+    <input class="${todoDis}" type="checkbox" id="checkbox-input" name="${todoDis}" />
+    <label for="${todoDis}">${todoDis}</label>
+  </div>
+  <div class="todo-bin">
+    <svg
+      width="15px"
+      color="#007bff"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 448 512"
+    >
+      <path
+        style="fill: rgb(0, 123, 255)"
+        d="M432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16zM53.2 467a48 48 0 0 0 47.9 45h245.8a48 48 0 0 0 47.9-45L416 128H32z"
+      ></path>
+    </svg>
+  </div>`;
+  
+  const bin = todo.querySelector(".todo-bin");
+  bin.addEventListener("click", () => {
+    todo.remove();
+  });
+  
+  
+    allTodos.append(todo);
+  })
+}
+
+function removeLocalTodos(todoDis){
+  let todos;
+  if (localStorage.getItem("myTodos") === null) {
+    todos = [];
+   
+  } else {
+    todos = JSON.parse(localStorage.getItem('myTodos'))
+    
+  }
+  console.log(todoDis)
 }
